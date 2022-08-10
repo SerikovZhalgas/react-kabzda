6e0597a051606a4303df91372e22995cd4694aa0
SerikovZhalgas <zhalgasserikov@gmail.com>
2022-08-11 00:32:51 +0600

    commit useMemo

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Select} from './Select';
+import React from 'react';
+
 
 
 export default {
@@ -8,9 +8,15 @@ export default {
 }
 
 const arr = [
-    {value: '1',title: 'Ereymentau'},
-    {value: '2',title: 'Nur-sultan'},
-    {value: '3',title: 'Almaty'},
+    {value: '1',title: 'Ereymentau', country:1, peopleCount:5000},
+    {value: '2',title: 'Nur-sultan', country:1, peopleCount:1000000},
+    {value: '3',title: 'Almaty', country:1, peopleCount:17000000},
+    {value: '4',title: 'Vancouver', country:2, peopleCount:675000 },
+    {value: '5',title: 'Toronto', country:2, peopleCount:30000000},
+    {value: '6',title: 'Ottawa', country:2, peopleCount:1000000},
+    {value: '7',title: 'New-York', country:3, peopleCount:84000000 },
+    {value: '8',title: 'Las-Vegas', country:3, peopleCount:645000},
+    {value: '9',title: 'Miami', country:3, peopleCount:461000},
 ]
 
 /*
@@ -30,3 +36,4 @@ export const withoutValueSelect = () => {
     />
 }
 */
+
---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,79 @@
+import React, {useMemo, useState} from 'react';
+
+export default {
+    title: 'useMemo',
+}
+
+export const DifficultCountingExample = () => {
+    const [a, setA] = useState<number>(5)
+    const [b, setB] = useState<number>(5)
+
+    let resultA = 1;
+    let resultB = 1;
+
+    resultA = useMemo(() => {
+        let temResultA = 1
+        for (let i = 1; i <= a; i++) {
+            let fake = 0
+            while (fake < 100000000) {
+                fake++
+                const fakeValue = Math.random()
+            }
+            temResultA *= i
+        }
+        return temResultA
+    }, [a])
+
+
+    for (let i = 1; i <= b; i++) {
+        resultB *= i
+    }
+
+    return <>
+        <input value={a} onChange={(e) => setA(+e.currentTarget.value)}/>
+        <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
+        <hr/>
+
+        <div>
+            Result for a: {resultA}
+        </div>
+        <div>
+            Result for b: {resultB}
+        </div>
+
+
+    </>
+}
+
+
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log('Users Secret')
+    return <div>
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
+    </div>
+}
+
+const Users = React.memo(UsersSecret)
+
+export const HelpsForReactMemo = () => {
+    console.log('HelpsForReactMemo')
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Dimych', 'Zhalgas', 'Vlad', 'Kamila'])
+
+    const newArray = useMemo(()=>{
+        const newArray =  users.filter(el=>el.toLowerCase().indexOf('a') > -1)
+        return newArray
+    },[users])
+
+    const addUser = () =>{
+        const newUsers = [...users, 'Kamila ' + new Date().getTime()]
+        setUsers(newUsers)
+    }
+
+    return <>
+        <button onClick={() => {setCounter(counter + 1)}}>+</button>
+        <button onClick={() => {addUser()}}>add user</button>
+        {counter}
+        <Users users={newArray}/>
+    </>
+}
